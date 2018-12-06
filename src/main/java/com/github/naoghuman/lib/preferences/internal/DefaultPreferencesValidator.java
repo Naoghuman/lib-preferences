08e89e1478f86d0a41163d9d172ccc38cafbdaa8
Naoghuman <peter.rogge@yahoo.de>
2018-12-06 05:31:40 +0100

    #32 Add unittests for the class DefaultPreferencesValidator.

--- src/main/java/com/github/naoghuman/lib/preferences/internal/DefaultPreferencesValidator.java
+++ src/main/java/com/github/naoghuman/lib/preferences/internal/DefaultPreferencesValidator.java
@@ -22,20 +22,22 @@ import java.util.Objects;
  * This class contains different general validation methods in context from the 
  * library {@code Lib-Preferences}.
  *
- * @since  0.6.0
- * @author Naoghuman
+ * @since   0.6.0
+ * @version 0.6.0
+ * @author  Naoghuman
  */
 public class DefaultPreferencesValidator {
     
     /**
      * Validates if the attribute {@code value} isn't {@code NULL}.
      *
-     * @param  value the attribute which should be validated.
-     * @param  <T>   the type of the reference.
-     * @throws NullPointerException if {@code (value == NULL)}.
-     * @since  0.6.0
-     * @author Naoghuman
-     * @see    java.lang.NullPointerException
+     * @param   value the attribute which should be validated.
+     * @param   <T>   the type of the reference.
+     * @throws  NullPointerException if {@code (value == NULL)}.
+     * @since   0.6.0
+     * @version 0.6.0
+     * @author  Naoghuman
+     * @see     java.lang.NullPointerException
      */
     public static <T> void requireNonNull(final T value) {
         Objects.requireNonNull(value, "The attribute [value] can't be NULL"); // NOI18N
@@ -44,13 +46,14 @@ public class DefaultPreferencesValidator {
     /**
      * Validates if the attribute {@code value} isn't {@code NULL} and not {@code EMPTY}.
      *
-     * @param  value the attribute which should be validated.
-     * @throws IllegalArgumentException if {@code (value.trim() == EMPTY)}.
-     * @throws NullPointerException     if {@code (value        == NULL)}.
-     * @since  0.6.0
-     * @author Naoghuman
-     * @see    java.lang.IllegalArgumentException
-     * @see    java.lang.NullPointerException
+     * @param   value the attribute which should be validated.
+     * @throws  IllegalArgumentException if {@code (value.trim() == EMPTY)}.
+     * @throws  NullPointerException     if {@code (value        == NULL)}.
+     * @since   0.6.0
+     * @version 0.6.0
+     * @author  Naoghuman
+     * @see     java.lang.IllegalArgumentException
+     * @see     java.lang.NullPointerException
      */
     public static void requireNonNullAndNotEmpty(final String value) {
         requireNonNull(value);
---
+++ src/test/java/com/github/naoghuman/lib/preferences/internal/DefaultPreferencesValidatorTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2018 Naoghuman's dream
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.lib.preferences.internal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * UnitTests to test the default implementation 
+ * {@link com.github.naoghuman.lib.i18n.internal.DefaultI18NValidator}.
+ *
+ * @since   0.6.0
+ * @version 0.6.0
+ * @author  Naoghuman
+ * @see    com.github.naoghuman.lib.i18n.internal.DefaultPreferencesValidator
+ */
+public class DefaultPreferencesValidatorTest {
+    
+    public DefaultPreferencesValidatorTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    
+    @Test
+    public void requireNonNull() {
+        String hello = "hello";
+        DefaultPreferencesValidator.requireNonNull(hello);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void requireNonNullThrowsNullPointerException() {
+        String hello = null;
+        DefaultPreferencesValidator.requireNonNull(hello);
+    }
+    
+    @Test
+    public void requireNonNullAndNotEmptyString() {
+        String hello = "hello";
+        DefaultPreferencesValidator.requireNonNullAndNotEmpty(hello);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void requireNonNullAndNotEmptyStringThrowsNullPointerException() {
+        String hello = null;
+        DefaultPreferencesValidator.requireNonNullAndNotEmpty(hello);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void requireNonNullAndNotEmptyStringThrowsIllegalArgumentException() {
+        String hello = "";
+        DefaultPreferencesValidator.requireNonNullAndNotEmpty(hello);
+    }
+    
+}
